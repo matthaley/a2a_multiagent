ccd7790f589d7c38a5b4d3dd85170dc4e04be965
matthew_haley <matthew_haley@kiongroup.com>
2025-12-12 21:50:52 +0000

    deployment for idp

--- horizon_agent/test_cloud_run.sh
+++
@@ -1,4 +0,0 @@
-curl http://0.0.0.0:10008/.well-known/agent-card.json
-
-curl -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://horizon-integration-test--horizon-agent-tenant-abc-833546053256.us-central1.run.app/.well-known/agent-card.json"
-
